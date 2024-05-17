2bd445ff5bb3c9112aa8dbf4e5de73e0813d06cc
Janith-Sandamal <pge.janithsandamal@gmail.com>
2024-05-17 23:22:34 +0530

    getCompaigns Function defined done ✅

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -55,6 +55,17 @@ contract CrowdFunding {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCompaigns() {}
+    function getCompaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+
+    }
 
 }
