5e27d1bf55555e5779ea83974d59c79f352f237b
Janith-Sandamal <pge.janithsandamal@gmail.com>
2024-05-19 14:15:38 +0530

    SmartContract wallet address changed✅

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0xe68C9537D3b95B72bc5764f7f6fb03a5605B4760');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
